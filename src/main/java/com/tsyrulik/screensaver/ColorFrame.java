f9fc9ab813c0bd4c004fe0124e76d0d82c386afa
brutal-snezhok <czyrulik97@mail.ru>
2020-06-14 17:01:31 +0300

    added screen saver package

---
+++ src/main/java/com/tsyrulik/screensaver/ColorFrame.java
@@ -0,0 +1,28 @@
+package com.tsyrulik.screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Component
+public class ColorFrame extends JFrame {
+
+    @Autowired
+    private Color color;
+
+    public ColorFrame() {
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace() {
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(700));
+        getContentPane().setBackground(color);
+        repaint();
+    }
+}
